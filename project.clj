cc2d0e945b8382c2b4fe77d3b1c7fd5be9b0c094
protoss-barcode <184213423+protoss-barcode@users.noreply.github.com>
2026-01-09 17:32:15 -0600

    Update project.clj
    There is no resources to copy

--- project.clj
+++ project.clj
@@ -1,4 +1,1 @@
-(defproject tcp "0.0.0"
-  :dependencies [[org.clojure/clojure "1.12.0"]]
-  :resource-paths ["resources"]
-  :min-lein-version "2.5.3")
+(defproject tcp "0.0.0" :dependencies [[org.clojure/clojure "1.12.0"]] :min-lein-version "2.5.3")
